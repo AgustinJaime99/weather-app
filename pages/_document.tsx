6f0575382060fb37794a3618e6b3321edf3e4535
AgustinJaime99 <agustindiegojaime@gmail.com>
2023-02-16 07:44:16 -0300

    Update: remove peer dependencies, style responsive, meta tags for https

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,10 +7,11 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        <body>
+          <Main />
+          <NextScript />
+        </body>
     </Html>
   )
 }
